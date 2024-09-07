ca674d6922666df26d4ff396242b52bb48835949
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2024-09-08 00:47:34 +0500

    Cal.py

--- Cal.py
+++ Cal.py
@@ -2,21 +2,30 @@ num1  = int(input("Enter Number1:\t"))
 num2  = int(input("Enter Number2: \t"))
 operator : str = input("Enter an operator (+, -, *, /): ")
 
-if operator == '+':
-    result = num1+num2
-elif operator == '-':
-    result = num1-num2
-elif operator == '*':
-    result = num1*num2
-elif operator == '/':
-    if num1 == 0 or num2 == 0:
-        print("Division by Zero")
+while operator!=0:
+    if operator == '+':
+        result = num1+num2
+        print(f"{num1} + {num2} = {result}")
+        break
+    elif operator == '-':
+        result = num1-num2
+        print(f"{num1} - {num2} = {result}")
+        break
+    elif operator == '*':
+        result = num1*num2
+        print(f"{num1} * {num2} = {result}")
+        break
+    elif operator == '/':
+       if num1 == 0 or num2 == 0:
+           print("Division by Zero")
+           break
+       else:
+           result = num1/num2
+           print(f"{num1} / {num2} = {result}")
+           break
     else:
-        result = num1/num2
-   
-else:
-    print("Invalid Operator")
-print("Result:",result)
+        print("Invalid Operator")
+        break
 
     
      
