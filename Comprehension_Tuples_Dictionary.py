28e16ed4bfa8ccd15c07f3d83000f65b01f7ca14
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2024-10-06 21:02:12 +0500

    List Comprehensions

--- Comprehension_Tuples_Dictionary.py
+++ Comprehension_Tuples_Dictionary.py
@@ -1,6 +1,14 @@
+# List Comprehensions
+
 n1 = int(input("Enter Number :"))
 n2 = int(input("Enter Number :"))
 squares : list[int]  = []
 for x in range(n1, n2):
     squares.append(x**2)
 print(squares)
+
+
+# Simplify Code with List Comprehensions
+
+squares : list[int] = [x**2 for x in range(1, 6)]
+print(squares)  
