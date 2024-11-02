312681277775490d582536293bbc2b6ed27c6a9f
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2024-11-02 17:14:48 +0500

    assignmrnts

--- .devcontainer/Assignments/assignment_01.py
+++
@@ -1,9 +0,0 @@
-def main():
-    num1 : str = input("Enter first number: ")
-    num1 : int = int(num1)
-    num2  : str = input("Enter second number: ")
-    num2 : int = int(num2)
-    total : int = num1 + num2
-    print("The total is " + str(total) + ".")
-if __name__ == '__main__':
-    main()
