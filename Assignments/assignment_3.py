a13204b9bc970ae95388e5fdb616cd35d861d528
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2024-11-02 17:59:10 +0500

    assignment_03

---
+++ Assignments/assignment_3.py
@@ -0,0 +1,14 @@
+def main():
+    a : int = 21  
+    b : int = 6 + a
+    c : int = 20 + b
+    d : int= c + a 
+    e : int = c  
+
+    print("Anton is " + str(a))
+    print("Beth is " + str(b))
+    print("Chen is " + str(c))
+    print("Drew is " + str(d))
+    print("Ethan is " + str(e))
+if __name__ == '__main__':
+    main()
