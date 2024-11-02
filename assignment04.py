057032bb09d2871cc08a1b979dc88182d155f01b
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2024-11-02 18:12:13 +0500

    assignment 04

---
+++ assignment04.py
@@ -0,0 +1,8 @@
+def triangle_perimeter():
+    side1 = float(input("Enter Length of side1: "))
+    side2 = float(input("Enter Length of side2: "))
+    side3 = float(input("Enter Length of side3: "))
+    print(f"Sum of All Sides: {side1 + side2 + side3}")
+
+if __name__ == '__main__':
+    triangle_perimeter()
